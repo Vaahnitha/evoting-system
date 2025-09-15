dece303a946a76fac6336d4a407ade16487454e5
vaahnitha <vaahnithachowdarychinthalapudi@gmail.com>
2025-09-15 12:47:19 +0530

    Fixed master user issues

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -8,9 +8,25 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Create superuser if it doesn't exist
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "AdminPassword#123")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+
